aa9c6c6a6537d3277074925ca060fdeda25fbdbf
ilhambony878-eng <ilhambony878@gmail.com>
2026-07-28 16:13:37 +0700

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -1,13 +1,24 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
+
 LOCAL_CPP_EXTENSION := .cpp .cc
+
 ifeq ($(TARGET_ARCH_ABI), armeabi-v7a)
-	LOCAL_MODULE := SAShaderL
+    LOCAL_MODULE := SAShaderL
 else
-	LOCAL_MODULE := SAShaderL64
+    LOCAL_MODULE := SAShaderL64
 endif
-LOCAL_SRC_FILES := main.cpp mod/logger.cpp
-LOCAL_CFLAGS += -O2 -mfloat-abi=softfp -DNDEBUG -std=c++14
+
+LOCAL_SRC_FILES := \
+    main.cpp \
+    mod/logger.cpp \
+    core/Phoenix.cpp
+
+LOCAL_CFLAGS += -O2
+LOCAL_CFLAGS += -DNDEBUG
+LOCAL_CFLAGS += -std=c++17
+
 LOCAL_LDLIBS += -llog
+
 include $(BUILD_SHARED_LIBRARY)
